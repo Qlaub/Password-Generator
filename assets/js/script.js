e31ebdcb8860964692ff8388e0be67c83ff1684d
Alex Glaubitz <e.pirazzi@gmail.com>
2022-03-06 03:43:51 -0500

    included characters prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-let passwordLength = function() {
+const passwordLength = function() {
   //prompt user for length of password
   let length = window.prompt("Choose password length (between 8 and 128 characters");
   length = parseInt(length);
@@ -15,8 +15,77 @@ let passwordLength = function() {
   return length;
 }
 
-let generatePassword = function() {
+const promptBuilder = function(characters, length) {
+
+  //start of prompt
+  let userPrompt = "You would like your password to include"
+
+  //build string based on number of choices selected
+  if (length === 1) {
+    userPrompt += ` ${characters[0]}?`
+    return userPrompt;
+  } else if (characters.length === 2) {
+    userPrompt += ` ${characters[0]} and ${characters[1]}?`
+  } else {
+    //append each choice one at a time
+    for (let i = 0; i < length-1; i++) {
+      userPrompt += ` ${characters[i]},`
+    }
+    //append last choice without comma and with question mark
+    userPrompt += ` and ${characters[length-1]}?`
+  }
+
+  return userPrompt;
+}
+
+const passwordCharacters = function() {
+  characters = []
+
+  //prompt user for lowercase letters
+  let lowercase = window.confirm("Would you like to include lowercase letters?")
+  if (lowercase) {
+    characters.push("lowercase letters")
+  }
+
+  //prompt user for uppercase letters
+  let uppercase = window.confirm("Would you like to include uppercase letters?")
+  if (uppercase) {
+    characters.push("uppercase letters")
+  }
+
+  //prompt user for numbers
+  let numeric = window.confirm("Would you like to include numbers?")
+  if (numeric) {
+    characters.push("numbers")
+  }
+
+  //prompt user for special characters
+  let special = window.confirm("Would you like to include special characters?")
+  if (special) {
+    characters.push("special characters")
+  }
+
+  if (characters.length === 0) {
+    window.alert("Please choose at least one option")
+    return passwordCharacters();
+  }
+
+  //confirm the users choices
+  let confirmPrompt = promptBuilder(characters, characters.length);
+  console.log(confirmPrompt);
+
+  let userConfirm = window.confirm(confirmPrompt);
+  //if user doesn't confirm, restart function
+  if (!userConfirm) {
+    return passwordCharacters();
+  }
+
+  return characters;
+}
+
+const generatePassword = function() {
   let length = passwordLength();
+  let characters = passwordCharacters();
 }
 
 // Get references to the #generate element
