ac766950ab8f3d4eecf3516f855efc9c2f9e38d8
Alex Glaubitz <e.pirazzi@gmail.com>
2022-03-06 02:40:55 -0500

    check for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
 
+let passwordLength = function() {
+  //prompt user for length of password
+  let length = window.prompt("Choose password length (between 8 and 128 characters");
+  length = parseInt(length);
+
+  //checks if password length is a valid number
+  if (!length || length > 128 || length < 8) {
+    window.alert("Please enter a valid number");
+    //recursively executes function again if not
+    return passwordLength();
+  }
+
+  return length;
+}
+
+let generatePassword = function() {
+  let length = passwordLength();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
